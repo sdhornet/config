a6b0f4a955a8cd5a8562aa61bb3106bfb5c10aae
sdhornet <76028034+sdhornet@users.noreply.github.com>
2023-08-10 12:21:02 -0500

    Update nomachine-fix.sh

--- nomachine-fix.sh
+++ nomachine-fix.sh
@@ -1,6 +1,6 @@
 dpkg --purge nomachine
 wget -q 'https://download.nomachine.com/download/8.8/Linux/nomachine_8.8.1_1_amd64.deb'
-dpkg -i nomachine_7.7.4_1_amd64.deb >/dev/null
+dpkg -i nomachine_8.8.1_1_amd64.deb >/dev/null
 sed -i.bak 's/#EnableNetworkBroadcast 1/EnableNetworkBroadcast 0/' /usr/NX/etc/server.cfg
 # #sed -i.bak 's/#EnableScreenBlanking 0/EnableScreenBlanking 1/' /usr/NX/etc/server.cfg
 # #sed -i.bak 's/#EnableLockScreen 0/EnableLockScreen 1/' /usr/NX/etc/server.cfg
