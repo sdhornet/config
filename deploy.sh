f0115f3ce3761bba8f715094f11b227ef8adf3ae
sdhornet <76028034+sdhornet@users.noreply.github.com>
2023-08-22 15:59:13 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -40,6 +40,7 @@ wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc
 source ~/.zshrc
+rm go1.21.0.linux-amd64.tar.gz
 go version
 
 #Install Evilnginx
